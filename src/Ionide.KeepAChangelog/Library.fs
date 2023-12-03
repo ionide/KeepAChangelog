4a1d224c528cacd80e046e6a2d894f733c1026ce
nojaf <florian.verdonck@outlook.com>
2023-12-03 17:28:52 +0100

    Include sectionless items in ToMarkdown.

--- src/Ionide.KeepAChangelog/Library.fs
+++ src/Ionide.KeepAChangelog/Library.fs
@@ -67,6 +67,7 @@ module Domain =
                     yield! section "Security" this.Security
                     for KeyValue(heading, lines) in this.Custom do
                         yield! section heading lines
+                    yield renderItems this.SectionLessItems
                 ]
 
     type Changelogs =
